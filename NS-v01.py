25db6915c169529353fac1c7136c11d923ec8ecd
acibiber53 <acibiber53@hotmail.com>
2020-02-26 08:22:02 +0800

    Added board visualization Added full line fillers Added total counter

--- NS-v01.py
+++ NS-v01.py
@@ -9,6 +9,9 @@ Student ID: 19456662
 #+First I will try to create fully fillings matrix for different sizes. We can get this number from use and print out the output
 #Make the board fill the fulls and nulls
 
+BLK='\u2588'
+
+# This is recursive method that finds all possible line filling combinations for given size
 def levelsearch(partnumber, size):
     if partnumber==1:
         return str(size)
@@ -25,7 +28,8 @@ def levelsearch(partnumber, size):
         small+=1
     return ','.join([i for i in k])
     
-    
+
+# This method finds all full methods for the board size    
 def fullfinder(parts,size):
     i=1
     result=list()
@@ -35,16 +39,80 @@ def fullfinder(parts,size):
     result=(','.join(result)).split(',')
     return result
     
-
+# This method writes board out
 def printing(board):
-    print('\n'.join([' '.join([elem for elem in row])for row in board]))
+    print('\n\n'.join(['  '.join([elem for elem in row])for row in board]))
 
-def filler(size, fulls):
-    board=[['0']*n for i in range(n)]
+
+# This method only fills the full lines at the beginning.
+def fillfull(board, fulls, blocks, total):
+
+    for i in range(n):
+        if blocks[i] in fulls:
+            parting=list(fulls[fulls.index(blocks[i])])
+            indexc=0
+            for item in parting:
+                for j in range(int(item)):
+                    board[i][indexc]=BLK # block character
+                    indexc+=1
+                if indexc<n:
+                    board[i][indexc]='X'
+                    indexc+=1
+    i+=1
+    limi=2*n
+    while i<limi:
+        col=i-n
+        if blocks[i] in fulls:
+            parting=list(fulls[fulls.index(blocks[i])])
+            indexr=0
+            for item in parting:
+                for j in range(int(item)):
+                    board[indexr][col]=BLK
+                    indexr+=1
+                if indexr<n:
+                    board[indexr][col]='X'
+                    indexr+=1
+        i+=1
+    t=0
+    for row in board:
+        t+=row.count(BLK)
+    print(t)
+    total-=t
     printing(board)
+    return board, total
+
+
+# This method will slowly fill the board
+def filler(size, blocks, total):
+    fulls=fullfinder(int((size+1)/2),size)
+    
+    board=[['_']*n for i in range(n)]
+
+    board, total=fillfull(board, fulls, blocks, total)
+    
+    
+
+# This method gets input from a txt file. First line is board size n . Next n line row values, next n line column values.
+def takeinputs():
+    with open('nono.txt','r') as file:
+        content=file.readlines()
+        n=int(content[0])
+        for i in range(len(content)):
+            content[i]=content[i].replace('\n','')
+        blocks=content[1:]
+        
+    for i in range(len(blocks)):
+        if ' ' in blocks[i]:
+            blocks[i]=blocks[i].replace(' ','')
+    total=0
+    for i in range(n):
+        total+=sum([int(i) for i in list(blocks[i])])
+       
+    return n, blocks, total
 
 if __name__=='__main__':
-    n=int(input("How big is this square, honey?"))
-    fulls=fullfinder(int((n+1)/2),n)
-    filler(n,fulls)
+    n, blocks, total=takeinputs()
+    
+    
+    filler(n, blocks, total)
     
